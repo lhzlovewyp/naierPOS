f406caaf14de0f563a7ec2d0c4eceaa232be979b
mishitwins1 <mishitwins1@163.com>
2016-07-11 09:01:13 +0800

    账户店铺

---
+++ naierPOS-rest/src/main/java/com/joker/common/service/AccountStoreService.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.joker.common.service;
+
+import com.joker.common.model.AccountStore;
+
+/**
+ * @author zhangfei
+ * 
+ */
+public interface AccountStoreService {
+
+	/**
+	 * 删除账户和门店关联信息.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public void deleteAccountStoreByID(String id);
+
+	/**
+	 * 修改账户和门店关联信息.
+	 * 
+	 * @param accountStore
+	 * @return
+	 */
+	public void updateAccountStore(AccountStore accountStore);
+
+	/**
+	 * 保存账户和门店关联信息.
+	 * 
+	 * @param accountStore
+	 * @return
+	 */
+	public void insertAccountStore(AccountStore accountStore);
+}
---
+++ naierPOS-rest/src/main/java/com/joker/common/service/impl/AccountStoreServiceImpl.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.joker.common.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joker.common.mapper.AccountStoreMapper;
+import com.joker.common.model.AccountStore;
+import com.joker.common.service.AccountStoreService;
+
+/**
+ * @author zhangfei
+ * 
+ */
+@Service
+public class AccountStoreServiceImpl implements AccountStoreService {
+
+	@Autowired
+	AccountStoreMapper mapper;
+
+	@Override
+	public void deleteAccountStoreByID(String id) {
+		mapper.deleteAccountStoreByID(id);
+
+	}
+
+	@Override
+	public void updateAccountStore(AccountStore accountStore) {
+		mapper.updateAccountStore(accountStore);
+
+	}
+
+	@Override
+	public void insertAccountStore(AccountStore accountStore) {
+		mapper.insertAccountStore(accountStore);
+
+	}
+}
