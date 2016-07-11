6ec4f39b8bb8e2884592c1da35288894e250f8bf
mishitwins1 <mishitwins1@163.com>
2016-07-11 08:59:50 +0800

    账号角色、店铺

---
+++ naierPOS-rest/src/main/java/com/joker/common/mapper/AccountRoleMapper.java
@@ -0,0 +1,37 @@
+package com.joker.common.mapper;
+
+import org.springframework.stereotype.Repository;
+
+import com.joker.common.model.AccountRole;
+import com.joker.core.annotation.DataSource;
+
+@Repository
+public interface AccountRoleMapper {
+
+	/**
+	 * 删除账户和门店关联信息.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@DataSource("master")
+	public void deleteAccountRoleByID(String id);
+
+	/**
+	 * 修改账户和门店关联信息.
+	 * 
+	 * @param accountStore
+	 * @return
+	 */
+	@DataSource("master")
+	public void updateAccountRole(AccountRole accountRole);
+
+	/**
+	 * 保存账户和门店关联信息.
+	 * 
+	 * @param accountStore
+	 * @return
+	 */
+	@DataSource("master")
+	public void insertAccountRole(AccountRole accountRole);
+}
